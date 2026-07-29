7df2c1844cbd99e2751eb9fd9829117340dc4e8a
mtaillo1-beep <mtaillo1@charlotte.edu>
2026-07-29 00:48:30 +0000

    Start over

--- react-blog-assignment/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="site-footer" id="contact">
-      <p>© {new Date().getFullYear()} SimpleStory Blog · Made for curious minds.</p>
-    </footer>
-  )
-}
-
-export default Footer
